6c5f915b1c5677c78e8ba801cbb5d9de324f95be
buxkk321 <buxkk321@users.noreply.github.com>
2014-09-17 16:53:49 +0800

    Update function.php

--- function.php
+++ function.php
@@ -144,3 +144,17 @@ function getDist($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo){
 	$angle=cos($latFrom)*cos($latTo)+cos($lonDelta)*sin($latFrom)*sin($latTo);
 	return $angle*6371000;
 }
+function vincentyGreatCircleDistance($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371000) {
+  // convert from degrees to radians
+  // From:www.uncletoo.com
+  $latFrom = deg2rad($latitudeFrom);
+  $lonFrom = deg2rad($longitudeFrom);
+  $latTo = deg2rad($latitudeTo);
+  $lonTo = deg2rad($longitudeTo);
+  $lonDelta = $lonTo - $lonFrom;
+  $a = pow(cos($latTo) * sin($lonDelta), 2) +
+    pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
+  $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
+  $angle = atan2(sqrt($a), $b);
+  return $angle * $earthRadius;
+}
